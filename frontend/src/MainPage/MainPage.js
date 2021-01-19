3790a83ae86b5da9bf4b93df1fda3eab0fdd996b
Brian Lee <brian891221@gmail.com>
2021-01-20 01:20:47 +0800

    Implement panelSwitcher on left Panel

--- frontend/src/MainPage/MainPage.js
+++ frontend/src/MainPage/MainPage.js
@@ -3,6 +3,9 @@ import ContextStore from "../ContextStore";
 import ChatroomButton from "./ChatroomButton/ChatroomButton";
 import "./MainPage.scss";
 import MatchPanel from "./Panel/MatchPanel";
+import ChatPanel from "./Panel/ChatPanel"
+import Tab from "react-bootstrap/Tab"
+import Tabs from "react-bootstrap/Tabs"
 
 const MainPage = () => {
 	// eslint-disable-next-line no-unused-vars
@@ -19,14 +22,20 @@ const MainPage = () => {
 					/>
 					<div className="profile-title">My Profile</div>
 				</div>
+				<Tabs id="panelSwitcher" activeKey={mode} onSelect={(k)=>setMode(k)}>
+					<Tab eventKey="match" title="Match"></Tab>
+					<Tab eventKey="chat" title="Chat"></Tab>
+				</Tabs>
 				<div className="messages">
+					{mode==="chat"?<div>
 					<ChatroomButton username="test1" lastMessage="test4" />
 					<ChatroomButton choosing username="test2" lastMessage="test5" />
-					<ChatroomButton username="test3" lastMessage="test6" />
+					<ChatroomButton username="test3" lastMessage="test6" /></div>:<div></div>}
+					
 				</div>
 			</div>
 			{
-				mode==="match"?<MatchPanel></MatchPanel>:<div></div>
+				mode==="match"?<MatchPanel></MatchPanel>:<ChatPanel></ChatPanel>
 			}	
 		</div>
 	);
---
+++ frontend/src/MainPage/Panel/ChatPanel.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ChatPanel = (props) => {
+    return (
+        <div className="right-panel">
+            <span>This is ChatPanel</span>
+        </div>
+    )
+}
+
+export default ChatPanel
