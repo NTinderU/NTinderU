c3f9cf9fe2210475a2927d7802e4ac07ca367928
Brian Lee <brian891221@gmail.com>
2021-01-20 00:17:33 +0800

    Extract MatchPanel component

--- frontend/src/MainPage/MainPage.js
+++ frontend/src/MainPage/MainPage.js
@@ -1,15 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ContextStore from "../ContextStore";
 import ChatroomButton from "./ChatroomButton/ChatroomButton";
-import MoodButton from "./MoodButton/MoodButton";
 import "./MainPage.scss";
-import ArrowBox from "../svg/ArrowBox";
-import Enter from "../svg/Enter";
-import Space from "../svg/Space";
+import MatchPanel from "./Panel/MatchPanel";
 
 const MainPage = () => {
 	// eslint-disable-next-line no-unused-vars
 	const { loggedInUser } = useContext(ContextStore);
+	const [mode, setMode] = useState("match");
 	return (
 		<div className="main">
 			<div className="left-panel">
@@ -28,42 +26,7 @@ const MainPage = () => {
 				</div>
 			</div>
 			<div className="right-panel">
-				<img
-					className="user-picture-big"
-					src="https://via.placeholder.com/320x500.png"
-					alt="帥哥 || 美女"
-				/>
-				<div className="mood-buttons">
-					<MoodButton type="x" />
-					<MoodButton type="star" />
-					<MoodButton type="heart" />
-				</div>
-				<footer className="keyboard-shortcuts">
-					<div>
-						<ArrowBox direction="left" />
-						<span>NOPE</span>
-					</div>
-					<div>
-						<ArrowBox direction="right" />
-						<span>LIKE</span>
-					</div>
-					<div>
-						<ArrowBox direction="top" />
-						<span>OPEN PROFILE</span>
-					</div>
-					<div>
-						<ArrowBox direction="bottom" />
-						<span>CLOSE PROFILE</span>
-					</div>
-					<div>
-						<Enter />
-						<span>SUPER LIKE</span>
-					</div>
-					<div>
-						<Space />
-						<span>NEXT PHOTO</span>
-					</div>
-				</footer>
+				<MatchPanel></MatchPanel>
 			</div>
 		</div>
 	);
