3729768a137fcd01e50255a8c5bf78af417ec917
春日　未来 <nicks96432@gmail.com>
2021-01-19 21:03:32 +0800

    main page typesetting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import IndexPage from "./IndexPage/IndexPage";
 import ContextStore from "./ContextStore";
-import ChatPage from "./ChatPage/ChatPage";
+import MainPage from "./MainPage/MainPage";
 const App = () => {
 	const [loggedIn, setLoggedIn] = useState(false);
 	const [loggedInUser, setLoggedInUser] = useState("");
 	return (
 		<ContextStore.Provider value={{ setLoggedIn, loggedInUser, setLoggedInUser }}>
-			{loggedIn ? <ChatPage /> : <IndexPage />}
+			{loggedIn ? <MainPage /> : <IndexPage />}
 		</ContextStore.Provider>
 	);
 };
