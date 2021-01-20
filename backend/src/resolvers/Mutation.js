1b9f8ee84d1487a745481dc62337df117195a2e4
Brian Lee <brian891221@gmail.com>
2021-01-20 14:12:56 +0800

    remove repeated resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -35,19 +35,6 @@ const Mutation = {
 		const updated = User.findOne({ username: username });
 		return updated;
 	},
-	createUser: async (parent, { data }, { User }) => {
-		let user = await User.findOne(data);
-		if (!user) {
-			user = new User(data);
-			await user.save();
-			return user;
-		} else return null;
-	},
-	deleteUser: async (parent, { data }, { User }) => {
-		const user = await User.findOne(data);
-		if (user) await User.deleteOne(data);
-		return user;
-	},
 	createChatroom: async (parent, { data }, { User, Chatroom }) => {
 		let room = new Chatroom(data);
 		let un1 = data.users[0],
