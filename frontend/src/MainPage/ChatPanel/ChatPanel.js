4274508e89600fbf9352279e691bd6fec0f56d79
春日　未来 <nicks96432@gmail.com>
2021-01-20 20:41:37 +0800

    chat frontend

--- frontend/src/MainPage/ChatPanel/ChatPanel.js
+++ frontend/src/MainPage/ChatPanel/ChatPanel.js
@@ -1,6 +1,37 @@
 import React from "react";
+import Submit from "../../svg/Submit";
 import "./ChatPanel.scss";
 
-const ChatPanel = () => <div className="chat-panel"></div>;
+const ChatPanel = () => (
+	<div className="chat-panel">
+		<div className="chat-header">
+			<img
+				className="profile-picture"
+				src="https://via.placeholder.com/100x100.png"
+				alt="profile"
+			/>
+			<div>blahblahblah</div>
+		</div>
+		<div className="chat-messages">
+			<div className="chat-message self">
+				<span> @@@@@@@@@@ </span>
+			</div>
+			<div className="chat-message">
+				<span> @@@@@@@@@@ </span>
+			</div>
+		</div>
+		<div className="chat-input">
+			<div className="chat-input-buttons"></div>
+			<div className="chat-input-wrapper">
+				<input type="text" placeholder="Aa" />
+			</div>
+			<div className="chat-input-submit">
+				<button className="chat-input-submit-button">
+					<Submit />
+				</button>
+			</div>
+		</div>
+	</div>
+);
 
 export default ChatPanel;
