477efe4c11d49b0835d80b00620f4089ce85110a
Jeff Tseng <b08902134@csie.ntu.edu.tw>
2021-01-20 20:28:27 +0800

    jie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import IndexPage from "./IndexPage/IndexPage";
 import ContextStore from "./ContextStore";
 import MainPage from "./MainPage/MainPage";
-import Imgur from "./UploadPage/Imgur"
 const App = () => {
 	const [loggedIn, setLoggedIn] = useState(true);
 	const [loggedInUser, setLoggedInUser] = useState("");
 	return (
-		<Imgur />
-		// <ContextStore.Provider value={{ setLoggedIn, loggedInUser, setLoggedInUser }}>
-		// 	{loggedIn ? <MainPage /> : <IndexPage />}
-		// </ContextStore.Provider>
+		<ContextStore.Provider value={{ setLoggedIn, loggedInUser, setLoggedInUser }}>
+			{loggedIn ? <MainPage /> : <IndexPage />}
+		</ContextStore.Provider>
 	);
 };
 
