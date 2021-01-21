0a3d6fa7e93f9e8ff7ece66eada29f80477776d8
春日　未来 <nicks96432@gmail.com>
2021-01-21 11:34:44 +0800

    link to heroku-backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { split, HttpLink } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 import App from "./App";
@@ -9,37 +9,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 const httpLink = new HttpLink({
-    uri: "http://localhost:4000/",
+	uri: "https://ntinderu-backend.herokuapp.com/",
 });
 
 // Create a WebSocket link:
 const wsLink = new WebSocketLink({
-    uri: `ws://localhost:4000/graphql`,
-    options: { reconnect: true },
+	uri: `ws://ntinderu-backend.herokuapp.com/graphql`,
+	options: { reconnect: true },
 });
 
 // using the ability to split links, you can send data to each link
 // depending on what kind of operation is being sent
 const link = split(
-    // split based on operation type
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === "OperationDefinition" &&
-            definition.operation === "subscription"
-        );
-    },
-    wsLink,
-    httpLink
+	// split based on operation type
+	({ query }) => {
+		const definition = getMainDefinition(query);
+		return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+	},
+	wsLink,
+	httpLink
 );
 
 const client = new ApolloClient({
-    link,
-    cache: new InMemoryCache().restore(),
+	link,
+	cache: new InMemoryCache().restore(),
 });
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById("root")
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById("root")
 );
